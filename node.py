8f92a18871d460a4b2fbd397d9cb82aa93eb5805
JacobHilbert <62489457+JacobHilbert@users.noreply.github.com>
2020-03-27 18:00:24 -0500

    Update node.py

--- node.py
+++ node.py
@@ -1,1 +1,23 @@
+from os import listdir
+from os.path import isdir as is_dir
 
+class Node:
+	# this could be a directory or a file
+	def __init__(self,path):
+		self.path = path
+		self.splitted = path.replace("./","").split("/")
+		self.name = self.splitted[-1]
+        self.level = len(self.splitted)-1
+        self.is_last = False
+		self.kind = "directory" if is_dir(self.path) else "file"
+	def __repr__(self):
+		return "<{}> {} at <{}>".format(self.name,self.kind,self.path[:-len(self.name)-1])
+	def __str__(self):
+		return "".join(self.splitted)
+	
+	
+	
+	
+	
+	
+	
