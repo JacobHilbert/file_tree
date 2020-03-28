ada0bda2057893f9a66b32fc83a510e7079051fe
JacobHilbert <jacob.hilbert.tree@gmail.com>
2020-03-27 22:29:58 -0500

    Added keyword arguments for path and only_dirs

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ from .tree import *
 from sys import argv
 
 
-def main(default,top_path,only_dirs):
+def main(default,top_path=".",only_dirs="False"):
     print(only_dirs)
     tree = Tree(top_path,only_dirs==True)
     tree.make_tree()
