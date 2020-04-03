b18f72c0b2e05f97f7e243c638e3250f7d31dd8e
JacobHilbert <jacob.hilbert.tree@gmail.com>
2020-04-03 02:53:35 -0500

    setup and gitignore after pip installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+name="file_tree",
+version="1.0",
+author="Jacob Hilbert",
+author_email="jacob.hilbert.tree@gmail.com",
+packages=["file_tree"],
+license="LICENSE",
+description="Non-recursive way to get GNU tree functionality",
+)
